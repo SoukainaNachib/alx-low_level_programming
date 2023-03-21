4c34907b59b62654dc072d599853d03d0fa960da
SoukainaNachib <soukaina.nachib23@gmail.com>
2023-03-21 11:10:54 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,12 @@
-#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - prints _putchar followed by new line
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
