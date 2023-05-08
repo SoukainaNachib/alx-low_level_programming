30efc7b2708860147b2a535466d02ef4ba196e72
SoukainaNachib <soukaina.nachib23@gmail.com>
2023-05-08 10:55:49 -0700

    main.h modification

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdio.h>
 int _putchar(char c);
